39dbe660cccdf9cbc12297a9fc40f53d312201cc
tamutamu <tamu.0.0.tamu@gmail.com>
2019-07-11 13:50:35 +0900

    Init.

---
+++ ubuntu18/base_install.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+##set -eux
+
+
+# パッケージのインストール時に、対話形式のユーザーインタフェースを抑制する
+export DEBIAN_FRONTEND=noninteractive
+
+# 日本国内のミラーサイトを使うようにし、ソースインデックスは取得しない
+sed -i \
+    -e 's,//archive.ubuntu.com/,//jp.archive.ubuntu.com/,g' \
+    -e '/^deb-src /s/^/#/' \
+    /etc/apt/sources.list
+
+
+INSTALL_PACKAGES="\
+    sudo \
+    wget \
+    language-pack-ja \
+    tzdata \
+    build-essential \
+    libssl-dev        \
+    libreadline-dev   \
+    zlib1g-dev        \
+    language-pack-ja  \
+    openssh-server    \
+    wget              \
+    zip unzip         \
+    tcpdump           \
+    vim               \
+    git               \
+    openjdk-8-jdk     \
+    maven             \
+    net-tools
+"
+apt update
+apt -y upgrade
+apt install -y systemd
+apt install -y --no-install-recommends ${INSTALL_PACKAGES}
+
+
+# ssh
+sed -i.bk -e 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+mkdir -p  ~/.ssh && \
+touch  ~/.ssh/authorized_keys && \
+chmod 600  ~/.ssh/authorized_keys && \
+echo "root:root" | chpasswd
+
+
+# proxy
+cat << EOT >> /etc/sudoers.d/proxy_env
+Defaults env_keep="http_proxy"
+Defaults env_keep+="https_proxy"
+Defaults env_keep+="HTTP_PROXY"
+Defaults env_keep+="HTTPS_PROXY"
+EOT
+
+cat << EOT >> /etc/profile.d/proxy.sh
+export http_proxy=${http_proxy}
+export https_proxy=${http_proxy}
+export HTTP_PROXY=${http_proxy}
+export HTTPS_PROXY=${http_proxy}
+EOT
+
+
+# ロケールを日本語にセットするが、メッセージ出力は翻訳しない
+LOCALTIME_FILE=/usr/share/zoneinfo/Asia/Tokyo
+update-locale LANG=ja_JP.UTF-8 LC_MESSAGES=C
+
+# 前のスクリプトでtzdataがインストールされていることを確認する
+if [ ! -f "${LOCALTIME_FILE}" ] ; then
+    echo "${LOCALTIME_FILE} does not exist."
+    exit 1
+fi
+
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+dpkg-reconfigure tzdata
+
+
+# python
+apt install -y \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline7 \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libncurses5 \
+    libncurses5-dev \
+    libncursesw5
+
+
+### pyenv
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+# Add init script to ~/.profile
+echo 'export PYENV_ROOT=$(echo ~/.pyenv)' >> ~/.profile
+echo 'export PATH="${PYENV_ROOT}/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+. ~/.profile
+
+
+### pyenv-virtualenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.profile
+. ~/.profile
+
+
+pyenv install 3.6.8
+pyenv virtualenv 3.6.8 py368
+pyenv global py368
+
---
+++ ubuntu18/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+base_name=os_ubuntu18
+port=2222
+
+rm -f ${base_name} ${base_name}.pub
+ssh-keygen -b 2048 -t rsa -f ./${base_name} -q -N ""
+
+docker run --privileged -d -p ${port}:22 --name ${base_name} ${base_name} /sbin/init
+sleep 3
+ip=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" ${base_name})
+
+ssh-copy-id -i ./${base_name}.pub -f root@${ip}
+
+echo "ssh -i ${base_name} root@${ip}" > ssh_${base_name}
+chmod +x ssh_${base_name}
