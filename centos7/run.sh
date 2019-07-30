53a85dcf78106e52e5762859e49f196cd2f41e23
tamutamu <tamu.0.0.tamu@gmail.com>
2019-07-30 11:41:01 +0900

    Update, centos7

--- centos7/run.sh
+++ centos7/run.sh
@@ -6,7 +6,9 @@ port=2222
 rm -f ${base_name} ${base_name}.pub
 ssh-keygen -b 2048 -t rsa -f ./${base_name} -q -N ""
 
-docker run --privileged -d -p ${port}:22 --name ${base_name} ${base_name} /sbin/init
+docker run --privileged -d -p ${port}:22 \
+  --mount type=bind,source="$(pwd)"/mount,target=/mnt/mount \
+  --name ${base_name} ${base_name} /sbin/init
 sleep 3
 ip=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" ${base_name})
 
